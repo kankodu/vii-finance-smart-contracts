b2bd46f5a4e032c66d0e392e5d40c0b01a358d9c
kankodu <reflectivecamouflage@gmail.com>
2025-05-27 13:30:57 +0530

    fix: deployment scripts for base

--- script/uniswap/UniswapV3Wrapper.s.sol
+++ script/uniswap/UniswapV3Wrapper.s.sol
@@ -4,8 +4,9 @@ pragma solidity ^0.8.13;
 import {Script, console} from "forge-std/Script.sol";
 import {UniswapV3Wrapper} from "src/uniswap/UniswapV3Wrapper.sol";
 import {BaseAddresses} from "script/BaseAddresses.sol";
+import {IEVault} from "lib/euler-interfaces/interfaces/IEVault.sol";
 
-contract CounterScript is Script {
+contract UniswapV3WrapperScript is Script {
     UniswapV3Wrapper public uniswapV3Wrapper;
 
     function setUp() public {}
@@ -13,15 +14,15 @@ contract CounterScript is Script {
     function run() public {
         vm.startBroadcast();
 
-        address poolAddress = 0x498581fF718922c3f8e6A244956aF099B2652b2b;
+        address poolAddress = 0xfBB6Eed8e7aa03B138556eeDaF5D271A5E1e43ef; //USDC / cbBTC v3 0.05% base pool
 
-        uniswapV3Wrapper = new UniswapV3Wrapper(
-            BaseAddresses.EVC,
-            BaseAddresses.POOL_MANAGER,
-            BaseAddresses.POSITION_MANAGER,
-            BaseAddresses.PERMIT2,
-            poolAddress
-        );
+        uniswapV3Wrapper = new UniswapV3Wrapper({
+            _evc: BaseAddresses.EVC,
+            _nonFungiblePositionManager: BaseAddresses.NON_FUNGIBLE_POSITION_MANAGER,
+            _oracle: IEVault(BaseAddresses.WETH_EVAULT).oracle(),
+            _unitOfAccount: IEVault(BaseAddresses.WETH_EVAULT).unitOfAccount(),
+            _poolAddress: poolAddress
+        });
 
         vm.stopBroadcast();
     }
--- script/uniswap/UniswapV4Wrapper.s.sol
+++ script/uniswap/UniswapV4Wrapper.s.sol
@@ -5,8 +5,9 @@ import {Script, console} from "forge-std/Script.sol";
 import {UniswapV4Wrapper} from "src/uniswap/UniswapV4Wrapper.sol";
 import {BaseAddresses} from "script/BaseAddresses.sol";
 import {PoolId} from "@uniswap/v4-core/src/types/PoolId.sol";
+import {IEVault} from "lib/euler-interfaces/interfaces/IEVault.sol";
 
-contract CounterScript is Script {
+contract UniswapV4WrapperScript is Script {
     UniswapV4Wrapper public uniswapV4Wrapper;
 
     function setUp() public {}
@@ -14,11 +15,15 @@ contract CounterScript is Script {
     function run() public {
         vm.startBroadcast();
 
-        PoolId poolId;
+        PoolId poolId = PoolId.wrap(0x96d4b53a38337a5733179751781178a2613306063c511b78cd02684739288c0a); //ETH / USDC v4 0.05% base
 
-        uniswapV4Wrapper = new UniswapV4Wrapper(
-            BaseAddresses.EVC, BaseAddresses.POOL_MANAGER, BaseAddresses.POSITION_MANAGER, BaseAddresses.PERMIT2, poolId
-        );
+        uniswapV4Wrapper = new UniswapV4Wrapper({
+            _evc: BaseAddresses.EVC,
+            _positionManager: BaseAddresses.POSITION_MANAGER,
+            _oracle: IEVault(BaseAddresses.WETH_EVAULT).oracle(),
+            _unitOfAccount: IEVault(BaseAddresses.WETH_EVAULT).unitOfAccount(),
+            _poolId: poolId
+        });
 
         vm.stopBroadcast();
     }
